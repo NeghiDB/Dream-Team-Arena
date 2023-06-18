dd27469f3df10cdd1cc86b4576353be20613ad23
Nosa <87219719+NeghiDB@users.noreply.github.com>
2023-06-18 02:19:08 +0100

    Update feedback.php

--- connections/feedback.php
+++ connections/feedback.php
@@ -2,21 +2,31 @@
     require_once "connection.php";
 
     session_start();
-    $userid = $_SESSION['userid'];
-    $comments = $_GET["comments"];
 
-    $sql = "INSERT INTO Feedback (Text, UserID) 
-            VALUES ('$comments', '$userid')";
+    if (isset($_SESSION['userid']) && isset($_GET['comments'])) {
+        $userid = $_SESSION['userid'];
+        $comments = $_GET['comments'];
 
-    $result = mysqli_query($conn, $sql);
-    if ($result === false) {
-        die('Query execution failed: ' . mysqli_error($conn));
+        // Prepare the SQL statement to prevent SQL injection
+        $sql = "INSERT INTO Feedback (Text, UserID) 
+                VALUES (?, ?)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("si", $comments, $userid);
+        
+        // Execute the prepared statement
+        if ($stmt->execute()) {
+            echo "<script>alert('Your feedback has been received.');location.href='../feedback.html';</script>";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+
+        // Close the statement
+        $stmt->close();
+    } else {
+        echo "Invalid request";
     }
 
-    if (mysqli_multi_query($conn, $sql)) {
-        echo "<script>alert('Your feedback has been received.');location.href='../feedback.html';</script>";
-    }
-    else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+    // Close the database connection
+    $conn->close();
 ?>
