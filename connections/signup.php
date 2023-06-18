418145947c6930db1319e997a960380221fb3081
Nosa <87219719+NeghiDB@users.noreply.github.com>
2023-06-18 02:21:20 +0100

    Update signup.php

--- connections/signup.php
+++ connections/signup.php
@@ -11,31 +11,46 @@
         $newpassword = $_POST["newpassword"];
         $confpassword = $_POST["confpassword"];
 
-        if($newpassword===$confpassword){
-            $sql = "INSERT INTO user (email, username, phonenumber, password)
-                VALUES ('$email', '$username', '$phone', '$confpassword')";
-            if (mysqli_query($conn, $sql)) {
-                echo "<script>
-                location.href='../index.html';
-                alert('Account created successfully.');
-                </script>";
-            }
-            elseif (mysqli_errno($conn)==1062){
-                echo "<script>
-                location.href='../signup.html';
-                alert('The email, phone number or username has already being used');
-                </script>";
-            }
-            else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
-        }else{
+        // Validate inputs
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             echo "<script>
             location.href='../signup.html';
-            alert('The passwords don`t match');
+            alert('Invalid email format.');
             </script>";
+            exit;
         }
-    }elseif (isset($_POST["update"])){
+
+        if ($newpassword !== $confpassword) {
+            echo "<script>
+            location.href='../signup.html';
+            alert('The passwords don`t match.');
+            </script>";
+            exit;
+        }
+
+        // Hash the password
+        $hashedPassword = password_hash($confpassword, PASSWORD_DEFAULT);
+
+        // Prepare and execute a parameterized INSERT statement
+        $stmt = $conn->prepare("INSERT INTO user (email, username, phonenumber, password)
+                                VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $email, $username, $phone, $hashedPassword);
+        if ($stmt->execute()) {
+            echo "<script>
+            location.href='../index.html';
+            alert('Account created successfully.');
+            </script>";
+        } elseif ($conn->errno == 1062) {
+            echo "<script>
+            location.href='../signup.html';
+            alert('The email, phone number, or username has already been used.');
+            </script>";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+
+        $stmt->close();
+    } elseif (isset($_POST["update"])){
         $userid1 = $userid;
         $username = $_POST["username"]; 
         $email = $_POST["email"];
@@ -45,31 +60,45 @@
         $newpassword = $_POST["newpassword"];
         $confpassword = $_POST["confpassword"];
 
-        if($newpassword===$confpassword){
-            $sql = "UPDATE user SET email = '$email', username = '$username', phonenumber = '$phone', password = '$confpassword',
-            accountnumber='$accountnumber', bankname='$bankname'
-            WHERE UserID = '$userid1'";
-            if (mysqli_query($conn, $sql)) {
-                echo "<script>
-                location.href='../settings.php';
-                alert('Account details updated successfully.');
-                </script>";
-            }
-            elseif (mysqli_errno($conn)==1062){
-                echo "<script>
-                location.href='../settings.php';
-                alert('The email, phone number or username has already being used');
-                </script>";
-            }
-            else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
-        }else{
+        // Validate inputs
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             echo "<script>
             location.href='../settings.php';
-            alert('The passwords don`t match');
+            alert('Invalid email format.');
             </script>";
+            exit;
         }
+
+        if ($newpassword !== $confpassword) {
+            echo "<script>
+            location.href='../settings.php';
+            alert('The passwords don`t match.');
+            </script>";
+            exit;
+        }
+
+        // Hash the password
+        $hashedPassword = password_hash($confpassword, PASSWORD_DEFAULT);
+
+        // Prepare and execute a parameterized UPDATE statement
+        $stmt = $conn->prepare("UPDATE user SET email = ?, username = ?, phonenumber = ?, password = ?, accountnumber = ?, bankname = ?
+                                WHERE UserID = ?");
+        $stmt->bind_param("ssssssi", $email, $username, $phone, $hashedPassword, $accountnumber, $bankname, $userid1);
+        if ($stmt->execute()) {
+            echo "<script>
+            location.href='../settings.php';
+            alert('Account details updated successfully.');
+            </script>";
+        } elseif ($conn->errno == 1062) {
+            echo "<script>
+            location.href='../settings.php';
+            alert('The email, phone number, or username has already been used.');
+            </script>";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+
+        $stmt->close();
     }
 
     require_once "disconnection.php";
